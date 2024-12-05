d44bbfaf4e4d7095e814da062aab43a27bd35b17
fabrizio.gabriele <fabrizio.gabriele@nttdata.com>
2024-12-05 17:26:32 +0100

    Day2 solved

--- src/main/kotlin/yr2024/day_04/Day04Ex2.kt
+++ src/main/kotlin/yr2024/day_04/Day04Ex2.kt
@@ -13,54 +13,16 @@ fun main(args: Array<String>) {
 
 fun countXMasOccurrences(matrix: List<CharArray>):Int {
     var count = 0
-    for(row in matrix.indices){
-        for(col in matrix[row].indices){
-            if(matrix[row][col]=='M'){
-                // START THE COUNT
-                if(row>=3 && col>=3 && isXFoundAt(row,col,matrix,"north-east"))
-                    count++;
-                if((matrix.size-1-col)>=3 && row>=3 && isXFoundAt(row,col,matrix,"north-west"))
-                    count++;
-                if((matrix.size-1-col)>=3 && (matrix[row].size-1-row)>=3 && isXFoundAt(row,col,matrix,"south-west"))
-                    count++;
-                if(col>=3 && (matrix[row].size-1-row)>=3 && isXFoundAt(row,col,matrix,"south-east"))
-                    count++;
+    for(row in 1..<matrix.size-1){
+        for(col in 1..<matrix[row].size-1){
+            if(matrix[row][col]=='A'){
+                if( (matrix[row-1][col-1] == 'M' || matrix[row+1][col+1] =='M') &&
+                    (matrix[row-1][col-1] == 'S' || matrix[row+1][col+1] =='S') &&
+                    (matrix[row+1][col-1] == 'M' || matrix[row-1][col+1] =='M') &&
+                    (matrix[row+1][col-1] == 'S' || matrix[row-1][col+1] =='S'))
+                    count++
             }
         }
     }
     return count
-}
-
-fun isXFoundAt(row: Int, col: Int, matrix: List<CharArray>, direction: String): Boolean{
-    when(direction){
-        "north-east" -> {
-            if(matrix[row-1][col-1] != 'A')
-                return false
-            if(matrix[row-2][col-2] != 'S')
-                return false
-            return (matrix[row-2][col] == 'M' || matrix[row][col-2] == 'M') && (matrix[row-2][col] == 'S' || matrix[row][col-2] == 'S')
-        }
-        "north-west" -> {
-            if(matrix[row-1][col+1] != 'A')
-                return false
-            if(matrix[row-2][col+2] != 'S')
-                return false
-            return (matrix[row-2][col] == 'M' || matrix[row][col+2] == 'M') && (matrix[row-2][col] == 'S' || matrix[row][col+2] == 'S')
-        }
-        "south-west" -> {
-            if(matrix[row+1][col+1] != 'A')
-                return false
-            if(matrix[row+2][col+2] != 'S')
-                return false
-            return (matrix[row+2][col] == 'M' || matrix[row][col+2] == 'M') && (matrix[row+2][col] == 'S' || matrix[row][col+2] == 'S')
-        }
-        "south-east" -> {
-            if(matrix[row+1][col-1] != 'A')
-                return false
-            if(matrix[row+2][col-2] != 'S')
-                return false
-            return (matrix[row+2][col] == 'M' || matrix[row][col-2] == 'M') && (matrix[row+2][col] == 'S' || matrix[row][col-2] == 'S')
-        }
-    }
-    return false
 }
